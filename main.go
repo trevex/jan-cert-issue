38ab888a4c2cf99d6b9cf57a52dc6c6bfd40247b
Niklas Voss <nvoss@google.com>
2022-07-29 11:52:15 +0200

    Use real bucket and list

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"io"
 	"log"
 
 	"gocloud.dev/blob"
@@ -10,16 +12,28 @@ import (
 
 func main() {
 	ctx := context.Background()
-	bucket, err := blob.OpenBucket(ctx, "gs://my-bucket")
+	bucket, err := blob.OpenBucket(ctx, "gs://nvoss-jan-problem")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer bucket.Close()
 	log.Print(bucket)
-	r, err := bucket.NewReader(ctx, "foo.txt", nil)
+	r, err := bucket.NewReader(ctx, "dog.png", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer r.Close()
 	log.Println("Content-Type:", r.ContentType())
+
+	iter := bucket.List(nil)
+	for {
+		obj, err := iter.Next(ctx)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(obj.Key)
+	}
 }
